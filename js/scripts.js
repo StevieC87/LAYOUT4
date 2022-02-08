2321d001e3c3e3494ab99ed6ad07508ef1bb7bcd
steviec1 <thestevieme@gmail.com>
2022-02-08 21:44:32 +0100

    menu etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,56 @@
 const hamburgerbutton = document.querySelector('#hamburger-menu');
+const menuhidden = document.getElementById("menuhidden");
+const visibleRight = menuhidden.classList.contains('visibleright');
+
+function reportWindowSize() {
+  /* et height = window.innerHeight; */
+  let width = window.innerWidth;
+
+  
+  if(width > 676) {
+   console.log(width, 'over 676');
+    menuhidden.classList.remove("visibleright");
+
+    
+  }
+  if(width < 676 ) {
+    console.log(width, 'under 676');
+    menuhidden.classList.remove("visibleright");
+
+   
+  }
+  if(!visibleRight && width > 676)
+    console.log(visibleRight, 'visibleright');
+      /* transformlinesWidth(); */
+      /*    */
+      document.querySelectorAll('.line').forEach((item) =>
+      item.classList.remove('openline')
+    );
+   
+ /*  alert(height); */
+}
+
+window.onresize = reportWindowSize;
 
 if (hamburgerbutton) {
 hamburgerbutton.addEventListener('click', function () {
   //  vis();
   slide();
   transformlines();
+ /*  document.querySelector('.line').style.backgroundColor = 'black'; */
 });
 }
 function slide() {
   const slideme = document.querySelector('#menuhidden');
-  const slidemeclasses = slideme.classList;
+  //boolean
+ /*  if(slidemeVisibleBoolean) {
+    menuhidden.classList.remove("visibleright");
+  }
+  else if (!slidemeVisibleBoolean) {
+    menuhidden.classList.add("visibleright");
+  }
+ */
+  const slidemeclasses = slideme.classList; 
   //  slideme.classList.replace('hiddenright', 'visibleright');
   const slidemeclassestoggle = slidemeclasses.toggle('visibleright');
 }
@@ -20,8 +61,25 @@ function transformlines() {
     //item.style.backgroundColor = "pink"
     item.classList.toggle('openline')
   );
+
 }
 
+
+function transformlinesWidth(){
+  let slidemeVisibleBoolean = menuhidden.classList.contains('visibleright');
+  if(slidemeVisibleBoolean) {
+    document.querySelectorAll('.line').forEach((item) =>
+    item.classList.remove('openline')
+  );
+  }
+  else if (!slidemeVisibleBoolean) {
+    document.querySelectorAll('.line').forEach((item) =>
+    item.classList.add('openline')
+    //item.classList.toggle('openline')
+    );
+ 
+}
+}
 
 
 /* ON WINDOW RESIZE, CLOSE HAMBURGER MENU IF STILL OPEN */
